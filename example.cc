cf761b315d0f6205508d97686e778ff3a1a7aa15
1024-- <1024--@users.noreply.github.com>
2021-09-01 02:27:16 +0700

    fix sizes

--- example.cc
+++ example.cc
@@ -5,7 +5,7 @@
 int main () {
   char buf[10] = "vo pitux";
   voreterator<1, 1> case_(buf, 2, 2);
-  voreterator<2, 1> word (buf+3, 7);
+  voreterator<2, 1> word (buf+3, 6);
   
   for (auto str : case_) {
     std::cout << str << std::endl;
